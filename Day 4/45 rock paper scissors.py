cde1c5fdea30c63d07abed07b9d0681e088757c0
Wouter Kursten <wouter@retouw.nl>
2021-01-07 17:49:02 +0100

    day 4b

--- Day 4/45 rock paper scissors.py
+++ Day 4/45 rock paper scissors.py
@@ -30,21 +30,20 @@ userchoicel = userchoice.lower()
 rockpaperscissors = ["rock","paper","scissors"]
 if not userchoicel in rockpaperscissors:
     print(f"{userchoice} is not a valid option")
-    exit()
-
-computerchoice = random.choice(rockpaperscissors)
-print(f"Computer chooses {computerchoice} ")
-if userchoicel == computerchoice:
-    print("Similar")
-elif userchoicel == "rock" and computerchoice == "paper":
-    print("Computer wins")
-elif userchoicel == "paper" and computerchoice == "scissors":
-    print("Computer wins")
-elif userchoicel == "scissors" and computerchoice == "rock":
-    print("Computer wins")
-elif userchoicel == "rock" and computerchoice == "scissors":
-    print("player wins")
-elif userchoicel == "paper" and computerchoice == "rock":
-    print("player wins")
-elif userchoicel == "scissors" and computerchoice == "paper":
-    print("player wins")
+else:
+    computerchoice = random.choice(rockpaperscissors)
+    print(f"Computer chooses {computerchoice} ")
+    if userchoicel == computerchoice:
+        print("Similar")
+    elif userchoicel == "rock" and computerchoice == "paper":
+        print("Computer wins")
+    elif userchoicel == "paper" and computerchoice == "scissors":
+        print("Computer wins")
+    elif userchoicel == "scissors" and computerchoice == "rock":
+        print("Computer wins")
+    elif userchoicel == "rock" and computerchoice == "scissors":
+        print("player wins")
+    elif userchoicel == "paper" and computerchoice == "rock":
+        print("player wins")
+    elif userchoicel == "scissors" and computerchoice == "paper":
+        print("player wins")
